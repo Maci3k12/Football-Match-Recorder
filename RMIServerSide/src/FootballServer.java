a1ad2046a759c8b207d8d16509ae2230956c0016
Maci3k12 <38101931+Maci3k12@users.noreply.github.com>
2018-08-01 08:17:34 +0200

    Initial commit

---
+++ RMIServerSide/src/FootballServer.java
@@ -0,0 +1,13 @@
+    import java.rmi.*;   
+     
+    public class FootballServer {
+    	   public static void main (String[] argv) {
+    		   try {
+    			   Football Hello = new Football();			   		   
+    			   Naming.rebind("rmi://localhost/ABC", Hello);
+    			   System.out.println("Serwer jest gotowy");
+    			   }catch (Exception e) {
+    				   System.out.println("Addition Server failed: " + e);
+    				}
+    		   }
+    }
---
+++ RMIServerSide/src/Football_Interface.java
@@ -0,0 +1,4 @@
+import java.rmi.*;
+     
+public interface Football_Interface extends Remote {
+}
